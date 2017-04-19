fd35f09d1ce768b55181de724955720b8e6fca62
Xi Chen <xic14@student.unimelb.edu.au>
2017-04-19 17:53:01 +1000

    Handlers: Update - Some necessary methods as a framework.
    A handler factory:
    Used to create handlers.
    
    Lists for resources and servers:
    Details, especially locks are not implemented.
    
    EZShareServer:
    Update - The lists of servers and resources are added.

---
+++ src/EZShare_Server/EZShareServer.java
@@ -0,0 +1,16 @@
+package EZShare_Server;
+
+/**
+ * Created by apple on 17/04/2017.
+ */
+public class EZShareServer {
+
+    ResourceList resourceList = new ResourceList();
+    ServerList serverList = new ServerList();
+
+    public EZShareServer(){
+
+    }
+
+
+}
