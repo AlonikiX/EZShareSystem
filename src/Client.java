26c2f301f66769ccaf52343ddfd2410d4b97c7ce
Xi Chen <xic14@student.unimelb.edu.au>
2017-04-29 21:13:18 +1000

    Fix IP validation Fix path, now, main classes are in package ESZShare

--- src/Client.java
+++ src/Client.java
@@ -1,45 +1,45 @@
-import CommonLibs.CommandLine.CliManager;
-import CommonLibs.CommandLine.OptionField;
-import CommonLibs.Commands.Command;
-import CommonLibs.Exception.UndefinedCommandException;
-import EZShare_Client.ClientSetting;
-import EZShare_Client.Processor.Processor;
-import org.apache.commons.cli.ParseException;
-import org.json.JSONObject;
-
-public class Client {
-    private static final String ROLE  = "CLIENT";
-    public static void main(String[] args) {
-
-        try{
-            //initial command line options
-            CliManager cliManager = new CliManager(ROLE);
-            cliManager.initOptions(args);
-
-            String log = ClientSetting.sharedSetting().getTime() +
-                    " - [EZShare.Client.main] - [INFO]- setting debug " +
-                    ((ClientSetting.sharedSetting().isDebugModel())?"on":"off");
-
-            //parse command
-            Command command = Command.commandFactory(cliManager);
-
-            //initial client setting
-            ClientSetting.sharedSetting().initSetting(cliManager);
-//        Communicator client = new Communicator();
-            Processor processor = Processor.processorFactory(command);
-            processor.process();
-            //connect to the server and start communication
-//        client.connectToServer();
-//        client.writeData(command.toJSON());
-//        client.readData();
-        } catch (UndefinedCommandException uce){
-            System.out.println("Received:\n");
-            System.out.println(uce.errorMessage());
-        } catch (ParseException e){
-            JSONObject obj = new JSONObject();
-            obj.put(OptionField.response.getValue(),OptionField.error.getValue());
-            obj.put(OptionField.errorMessage.getValue(),"invalid command");
-            System.out.println(obj.toString());
-        }
-    }
-}
+//import CommonLibs.CommandLine.CliManager;
+//import CommonLibs.CommandLine.OptionField;
+//import CommonLibs.Commands.Command;
+//import CommonLibs.Exception.UndefinedCommandException;
+//import EZShare_Client.ClientSetting;
+//import EZShare_Client.Processor.Processor;
+//import org.apache.commons.cli.ParseException;
+//import org.json.JSONObject;
+//
+//public class Client {
+//    private static final String ROLE  = "CLIENT";
+//    public static void main(String[] args) {
+//
+//        try{
+//            //initial command line options
+//            CliManager cliManager = new CliManager(ROLE);
+//            cliManager.initOptions(args);
+//
+//            String log = ClientSetting.sharedSetting().getTime() +
+//                    " - [EZShare.Client.main] - [INFO]- setting debug " +
+//                    ((ClientSetting.sharedSetting().isDebugModel())?"on":"off");
+//
+//            //parse command
+//            Command command = Command.commandFactory(cliManager);
+//
+//            //initial client setting
+//            ClientSetting.sharedSetting().initSetting(cliManager);
+////        Communicator client = new Communicator();
+//            Processor processor = Processor.processorFactory(command);
+//            processor.process();
+//            //connect to the server and start communication
+////        client.connectToServer();
+////        client.writeData(command.toJSON());
+////        client.readData();
+//        } catch (UndefinedCommandException uce){
+//            System.out.println("Received:\n");
+//            System.out.println(uce.errorMessage());
+//        } catch (ParseException e){
+//            JSONObject obj = new JSONObject();
+//            obj.put(OptionField.response.getValue(),OptionField.error.getValue());
+//            obj.put(OptionField.errorMessage.getValue(),"invalid command");
+//            System.out.println(obj.toString());
+//        }
+//    }
+//}
---
+++ src/EZShare/Server.java
@@ -0,0 +1,90 @@
+package EZShare;
+import CommonLibs.CommandLine.CliManager;
+import CommonLibs.Communication.Communicator;
+import CommonLibs.DataStructure.ServerListManager;
+import EZShare_Server.Dispatcher;
+import EZShare_Server.LimitedIPAddressListManager;
+import EZShare_Server.ServerSetting;
+import org.apache.commons.cli.ParseException;
+
+import javax.net.ServerSocketFactory;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by apple on 17/04/2017.
+ */
+public class Server {
+
+    private static final String ROLE  = "SERVER";
+    private Dispatcher dispatcher;
+
+    public static void main(String[] args) {
+        //initial command line options
+        CliManager cliManager = new CliManager(ROLE);
+        try {
+            cliManager.initOptions(args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //initial server setting
+        ServerSetting setting = ServerSetting.sharedSetting();
+        setting.initSetting(cliManager);
+
+        // print server setting information
+        String log = setting.getTime() + " - [EZShare.Server.main] " +
+                "- [INFO] Starting the EZShare Server";
+        System.out.println(log);
+        log = setting.getTime() + " - [EZShare.ServerSetting] " +
+                "- [INFO] - using secret: " + setting.getSecret();
+        System.out.println(log);
+        log = setting.getTime() + " - [EZShare.ServerSetting] " +
+                "- [INFO] - using advertised hostname: " + setting.getAdvertisedHostName();
+        System.out.println(log);
+        log = setting.getTime() + " - [EZShare.ServerSetting] " +
+                "- [INFO] - bound to por " + setting.getPort();
+        System.out.println(log);
+        log = setting.getTime() + " - [EZShare.ServerSetting] " +
+                "- [INFO] - setting debug " + ((setting.isDebugModel())?"on":"off");
+        System.out.println(log);
+
+        //initial server list manager
+        ServerListManager serverListManager = ServerListManager.sharedServerListManager();
+
+        //initial limited client list manager
+        LimitedIPAddressListManager limitedIPAddressListManager = LimitedIPAddressListManager.shareClientListManager();
+
+        //run server
+        ServerSocketFactory factory = ServerSocketFactory.getDefault();
+        try(ServerSocket serverSocket = factory.createServerSocket(ServerSetting.sharedSetting().getPort())){
+            System.out.println("Waiting for client connection..");
+            // Wait for connections.
+            while(true){
+                //set communicator
+                Communicator communicator = new Communicator(ServerSetting.sharedSetting());
+                communicator.establishConnection(serverSocket.accept());
+
+                String host = communicator.getClientAddress();
+                System.out.println("Server Information:");
+                System.out.println("connected with IP address:" + host);
+                if (limitedIPAddressListManager.limitConnection(host)) {
+                    System.out.println("Server Information:");
+                    System.out.println("connection too frequent");
+                    limitedIPAddressListManager.addIntervalLimitedIPAddress(host);
+                    continue;
+                }
+                limitedIPAddressListManager.addIntervalLimitedIPAddress(host);
+
+                //new dispatching thread
+                Dispatcher dispatcher = new Dispatcher();
+                dispatcher.bindCommunicator(communicator);
+                dispatcher.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
